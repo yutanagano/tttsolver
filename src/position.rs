93dabc4ec48fef1d580c4533bf331870397a5037
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2023-07-08 18:59:53 +0100

    Implement unit tests for position module

--- src/position.rs
+++ src/position.rs
@@ -1,5 +1,6 @@
 const BOARD_SIZE: usize = 3;
 
+#[derive(Debug, PartialEq)]
 enum Player {
     Noughts,
     Crosses
@@ -14,6 +15,7 @@ impl Player {
     }
 }
 
+#[derive(Debug, PartialEq)]
 enum GameState {
     InProgress,
     Lost(i8),
@@ -235,5 +237,97 @@ pub fn explore_and_print(position: &Position, current_tpgn: &str) {
                 explore_and_print(&next_position, &next_tpgn);
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{GameState, Player, Position};
+
+    #[test]
+    fn new_position_initialised_correctly() {
+        let position = Position::new();
+
+        assert_eq!(position.board, [[0;super::BOARD_SIZE];super::BOARD_SIZE]);
+        assert_eq!(position.player, Player::Noughts);
+        assert_eq!(position.moves_played, 0);
+        assert_eq!(position.state, GameState::InProgress);
+    }
+
+    #[test]
+    fn position_from_tpgn_created_correctly() {
+        let position = Position::from_tpgn("001122");
+
+        assert_eq!(
+            position.board,
+            [[1,0,0],
+             [0,2,0],
+             [0,0,1]]
+        );
+        assert_eq!(position.player, Player::Crosses);
+        assert_eq!(position.moves_played, 3);
+        assert_eq!(position.state, GameState::InProgress);
+    }
+
+    #[test]
+    #[should_panic]
+    fn bad_tpgn_causes_panic() {
+        Position::from_tpgn("0");
+    }
+
+    #[test]
+    fn test_can_play() {
+        let position = Position::from_tpgn("001122");
+
+        assert_eq!(position.can_play(0,0), false);
+        assert_eq!(position.can_play(0,1), true);
+    }
+
+    #[test]
+    fn test_play() {
+        let position = Position::from_tpgn("001122");
+        let position = position.play(0,1);
+
+        assert_eq!(
+            position.board,
+            [[1,2,0],
+             [0,2,0],
+             [0,0,1]]
+        );
+        assert_eq!(position.player, Player::Noughts);
+        assert_eq!(position.moves_played, 4);
+        assert_eq!(position.state, GameState::InProgress);
+    }
+
+    #[test]
+    #[should_panic]
+    fn illegal_move_causes_panic() {
+        let position = Position::from_tpgn("001122");
+        position.play(0,0);
+    }
+
+    #[test]
+    fn loss_detected() {
+        let position = Position::from_tpgn("0001110222");
+
+        assert_eq!(position.state, GameState::Lost(-3));
+    }
+
+    #[test]
+    fn draw_detected() {
+        let position = Position::from_tpgn("001122012120021210");
+
+        assert_eq!(position.state, GameState::Drawn);
+    }
+
+    #[test]
+    fn test_negamax() {
+        let mut counter: u32 = 0;
+        let position = Position::from_tpgn("00112201212002");
+
+        let score = super::negamax(&position, &mut counter);
+
+        assert_eq!(score, 0);
+        assert_eq!(counter, 5);
     }
 }
